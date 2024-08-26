92c4a1768959d284a01761d0bc8fefdfdca4ea1f
srinathk47 <srinathkadiyala472@gmail.com>
2024-08-27 00:26:56 +0530

    varibles

--- 5variable.sh
+++ 5variable.sh
@@ -5,3 +5,9 @@ echo "please enter your name::"
 read  USERNAME
 
 echo "Username entered is: $USERNAME"
+
+echo "please enter your password"
+
+read -s PASSWORD
+
+echo "passsword enetred is: $PASSWORD"

0b3772780a255da94fc379e536288d497b9d8bd8
srinathk47 <srinathkadiyala472@gmail.com>
2024-08-27 00:36:15 +0530

    varibles

---
+++ 6array.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#index starts from 0
+
+FRUITS=("apple","orange") #array
+
+echo "first fruit is: ${FRUITS[0]}"
+
+echo "second fruit is: ${FRUITS[1]}"
+
+echo "First fruit are is :${FRUITS[@]}"

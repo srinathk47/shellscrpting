24fa061d93dd39ea35ff43ad91795ec53fdef825
srinathk47 <srinathkadiyala472@gmail.com>
2024-08-27 00:01:55 +0530

    varibles

--- secondscript.sh
+++ secondscript.sh
@@ -2,3 +2,4 @@
 
 echo "ramesh:: hi suresh,how are you?"
 echo "suresh:: am fine"
+echo "ramesh:: what about updates"
---
+++ thirdscript.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=Raju
+PERSON2=Rakesh
+
+echo "$PERSON1:: hi $PERSON2,how are you?"
+echo "$PERSON2:: am fine"
+echo "$PERSON1:: what about updates"
